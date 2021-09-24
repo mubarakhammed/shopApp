d41e4fa8880eeabcdaeb1b2344016e679452dec2
mubarakhammed <mubarakhammed00@gmail.com>
2021-09-24 20:05:05 +0100

    added cart and major upgrade

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../widgets/badge.dart';
+import '../widgets/products_grid.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: <Widget>[
+          Expanded(
+            child: PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorites) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                });
+              },
+              icon: Icon(
+                Icons.more_vert,
+              ),
+              itemBuilder: (_) => [
+                PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites,
+                ),
+                PopupMenuItem(
+                  child: Text('Show All'),
+                  value: FilterOptions.All,
+                ),
+              ],
+            ),
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch!,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
